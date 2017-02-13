a91c0bd4e3fdeaf3cfbcde72ed16f6580dddb5f3
Vlad Manolache <vlad.manolache@endava.com>
2017-02-13 15:17:46 +0200

    Added MinStack and MinStack unit tests to project.

---
+++ src/algorithms/stack/MinStack.java
@@ -0,0 +1,51 @@
+package algorithms.stack;
+
+import java.util.Stack;
+
+/**
+ * Implement a stack with min() function, which will return the smallest number in the stack.
+ * It should support push, pop and min operation all in O(1) cost.
+ *
+ * Example:
+ * push(1)
+ * pop()   // return 1
+ * push(2)
+ * push(3)
+ * min()   // return 2
+ * push(1)
+ * min()   // return 1
+ * 
+ * @author Vlad Manolache
+ */
+public class MinStack {
+    
+    private final Stack<Integer> stack;
+    private final Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int value) {
+        if ( minStack.isEmpty() || minStack.peek() > value ) {
+            minStack.push(value);
+        }
+        stack.add(value);
+    }
+    
+    public int pop() {        
+        int value = stack.pop();
+        if ( value == min() ) {
+            minStack.pop();
+        }
+        return value;
+    }
+    
+    public int min() {
+        if ( minStack.isEmpty() ) {
+            return Integer.MAX_VALUE;
+        }
+        return minStack.peek();
+    }
+}
---
+++ test/algorithms/stack/MinStackTest.java
@@ -0,0 +1,31 @@
+package algorithms.stack;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Vlad Manolache
+ */
+public class MinStackTest {
+    
+    @Test
+    public void mainTest() {
+        MinStack minStack = new MinStack();
+        minStack.push(1);
+        minStack.pop();
+        minStack.push(2);
+        minStack.push(3);
+        assert(minStack.min() == 2);
+    }
+    
+    @Test
+    public void secondTest() {
+        MinStack minStack = new MinStack();
+        minStack.push(1);
+        minStack.pop();
+        minStack.push(2);
+        minStack.push(3);
+        minStack.push(1);
+        assert(minStack.min() == 1);
+    }
+}
